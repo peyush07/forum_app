8eb38f59de3859db482d50252be97fa995bece56
peyush07 <peyush707@gmail.com>
2021-06-24 21:49:59 +0530

    first commit

---
+++ src/components/vote-buttons.js
@@ -0,0 +1,107 @@
+
+import { IconButton, Text, VStack } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { FiArrowDown, FiArrowUp } from "react-icons/fi";
+import db from "../lib/firebase";
+
+const VoteButtons = ({ post }) => {
+  const [isVoting, setVoting] = useState(false);
+  const [votedPosts, setVotedPosts] = useState([]);
+
+  useEffect(() => {
+    // Fetch the previously voted items from localStorage. See https://stackoverflow.com/a/52607524/1928724 on why we need "JSON.parse" and update the item on localStorage. Return "true" if the user has already voted the post.
+    const votesFromLocalStorage = localStorage.getItem("votes") || [];
+    let previousVotes = [];
+
+    try {
+      // Parse the value of the item from localStorage. If the value of the
+      // items isn't an array, then JS will throw an error.
+      previousVotes = JSON.parse(votesFromLocalStorage);
+    } catch (error) {
+      console.error(error);
+    }
+
+    setVotedPosts(previousVotes);
+  }, []);
+
+  const handleDisablingOfVoting = (postId) => {
+    // This function is responsible for disabling the voting button after a
+    // user has voted. Fetch the previously voted items from localStorage. See
+    // https://stackoverflow.com/a/52607524/1928724 on why we need "JSON.parse"
+    // and update the item on localStorage.
+    const previousVotes = votedPosts;
+    previousVotes.push(postId);
+
+    setVotedPosts(previousVotes);
+
+    // Update the voted items from localStorage. See https://stackoverflow.com/a/52607524/1928724 on why we need "JSON.stringify" and update the item on localStorage.
+    localStorage.setItem("votes", JSON.stringify(votedPosts));
+  };
+  const handleClick = async (type) => {
+    setVoting(true);
+    // Do calculation to save the vote.
+    let upvotesCount = post.upvotesCount;
+    let downvotesCount = post.downvotesCount;
+
+    const date = new Date();
+
+    if (type === "upvote") {
+      upvotesCount = upvotesCount + 1;
+    } else {
+      downvotesCount = downvotesCount + 1;
+    }
+
+    await db.collection("posts").doc(post.id).set({
+      title: post.title,
+      upvotesCount,
+      downvotesCount,
+      createdAt: post.createdAt,
+      updatedAt: date.toUTCString(),
+    });
+
+    handleDisablingOfVoting(post.id);
+    setVoting(true);
+  };
+  const checkIfPostIsAlreadyVoted = () => {
+    if (votedPosts.indexOf(post.id) > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <>
+      <VStack>
+        <IconButton
+          size="lg"
+          colorScheme="purple"
+          aria-label="upvote"
+          icon={<FiArrowUp />}
+          onClick={() => handleClick("upvote")}
+          isLoading={isVoting}
+          isDisabled={checkIfPostIsAlreadyVoted()}
+        />
+        <Text bg="gray.100" rounded="md" w="100%" p={1}>
+          {post.upvotesCount}
+        </Text>
+      </VStack>
+      <VStack>
+        <IconButton
+          size="lg"
+          colorScheme="yellow"
+          aria-label="downvote"
+          icon={<FiArrowDown />}
+          onClick={() => handleClick("downvote")}
+          isLoading={isVoting}
+          isDisabled={checkIfPostIsAlreadyVoted()}
+        />
+        <Text bg="gray.100" rounded="md" w="100%" p={1}>
+          {post.downvotesCount}
+        </Text>
+      </VStack>
+    </>
+  );
+};
+
+export default VoteButtons;
